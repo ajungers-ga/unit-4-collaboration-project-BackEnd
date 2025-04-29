e61548947ae3f57eacc5352dfdd55c510cb6142c
seralex <alex.jungers@gmail.com>
2025-04-29 11:22:09 -0500

    updated model for reading_materials_api

--- reading_materials_api/models.py
+++ reading_materials_api/models.py
@@ -1,3 +1,18 @@
+# models.py
+
+# 1. Import Django's model base class
 from django.db import models
 
-# Create your models here.
+# 2. Define the model (ReadingMaterial)
+class ReadingMaterial(models.Model):
+    title = models.CharField(max_length=255)  # Short text field (e.g., "Harry Potter")
+    author = models.CharField(max_length=255)  # Short text field (e.g., "J.K. Rowling")
+    description = models.TextField()  # Long text field (e.g., summary of the book)
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when created
+    updated_at = models.DateTimeField(auto_now=True)  # Timestamp when updated
+
+#BELOW = # Dunder method (double underscore method).
+# Django uses __str__ to control what gets displayed when the object is shown as text.
+# This improves the Admin view — instead of showing 'Company object (1)', it shows a readable name like 'Apple' or 'Google'.
+    def __str__(self):
+        return self.title
