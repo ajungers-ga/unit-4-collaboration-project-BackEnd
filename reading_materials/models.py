749ded81702ea8925ade0289bb992204cf05d350
seralex <alex.jungers@gmail.com>
2025-05-02 10:52:14 -0500

    Add Author model, serializer, view, and route

--- reading_materials/models.py
+++ reading_materials/models.py
@@ -18,9 +18,20 @@ class ReadingMaterial(models.Model):
     readingStatus = models.CharField(max_length=50, default='unread')  # or whatever default fits
     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when created
     updated_at = models.DateTimeField(auto_now=True)  # Timestamp when updated
+    
 
 # BELOW = # Dunder method (double underscore method).
 # Django uses __str__ to control what gets displayed when the object is shown as text.
 # This improves the Admin view — instead of showing 'Company object (1)', it shows a readable name like 'Apple' or 'Google'.
     def __str__(self):
         return self.title
+    
+    # 2. Define the Author model
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.URLField()  # Image URL
+    biography = models.TextField()
+    popular_books = models.JSONField(default=list)  # List of book IDs
+
+    def __str__(self):
+        return self.name
